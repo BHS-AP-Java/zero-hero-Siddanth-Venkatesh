18e89f9cc6e950983f706c7dcd86a618d1e77c48
2001875 <98480402+2001875@users.noreply.github.com>
2025-09-12 16:46:33 +0000

    I am mastering print

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,13 +1,19 @@
-// Student name
-// class period
-// project name
-// date
+// Siddanth Venkatesh
+// P2
+// Zero Hero
+// 9/12
 
 /*
- * DESCRIPTION:
- * INPUT:
- * OUTPUT:
- * EDGE CASE:
+ * DESCRIPTION: Whatever Mr. Reiber Says
+ * INPUT: No inputs required
+ * OUTPUT: An email from a Nigerian Prince
+ * EDGE CASE: They are all edge cases at the moment
  */
 
 package edu.bhscs;
+
+public class Main{
+  public static void main(String[] args) {
+    System.out.println("Java wastes hours of someones life");
+  }
+}
