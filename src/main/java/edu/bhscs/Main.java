fd691f7172e09d1e63684a7f2e3a38e4e71f013a
2001875 <98480402+2001875@users.noreply.github.com>
2025-09-15 17:02:14 +0000

    I made the project given on schoology. I also started making Tic-Tac-Toe in the terminal, but couldn't finish due to time constraints and being a slow coder.

--- src/main/java/edu/bhscs/Main.java
+++ src/main/java/edu/bhscs/Main.java
@@ -1,22 +1,60 @@
 // Siddanth Venkatesh
 // P2
-// Zero Hero
+// Varriables: Replacement is the gateway to abstraction
 // 9/12
 
 /*
- * DESCRIPTION: Whatever Mr. Reiber Says
+ * DESCRIPTION: Test to show types in Java
  * INPUT: No inputs required
- * OUTPUT: An email from a Nigerian Prince
- * EDGE CASE: They are all edge cases at the moment
+ * OUTPUT: Shows Strings, ints, and other types
+ * EDGE CASE: None
  */
 
 package edu.bhscs;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
+  // This is the example of the char constant
+  // Because it is global, it is in capitial letters
+  public static final char A = 'A';
   public static void main(String[] args) {
-    System.out.println("-------------");
-    System.out.println("Java wastes hours of someones life");
-    System.out.println("Hello");
-    System.out.println("FIRST PROGRAM BRO");
+    int someNumber = 67;
+
+    System.out.println("67!" + A + someNumber);
+    System.out.println(A + "?" + A + someNumber);
+    System.out.println(A + A + someNumber);
+    System.out.println();
+    System.out.println("this is neet");
+    System.out.println();
+    System.out.println();
+    System.out.println("OK I am done");
+    char[][] board = create_board();
+    print_board(board);
+  }
+  public static void print_board(char[][] board){
+    for(int i = 0; i < 3; i++){
+      for(int j = 0; j < 3; j++){
+        System.out.print(board[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+  public static char[][] create_board(){
+    char[][] board = new char[3][3];
+    Arrays.stream(board).forEach(row -> Arrays.fill(row, '-'));
+    return board;
+  }
+  public static void turn(char[][] board, boolean player){
+    System.out.println(player ? "Place an X at": "Place a Y at");
+    // Scanner scanner = new Scanner(System.in);
+    // do{
+    //   String input = scanner.nextLine();
+    //  } while (
+    //   input.length() == 3,
+    //   input.contains(',')
+
+    //  );
   }
 }
