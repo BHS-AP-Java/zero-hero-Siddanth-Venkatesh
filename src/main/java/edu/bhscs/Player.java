ab196cd8cc614eb6790b136fdf7101a9bb57782e
2001875 <98480402+2001875@users.noreply.github.com>
2025-10-06 02:11:51 +0000

    Added some Scanner stuff

--- src/main/java/edu/bhscs/Player.java
+++ src/main/java/edu/bhscs/Player.java
@@ -20,14 +20,13 @@ public class Player {
     this.name = name;
     this.s = new Scanner(System.in);
     // These are the options the Player has
-
-    options.add(new Option<>("Make Customer"). this:makeCustomer);
+    options.add(new Option<Customer>(("Make Customer"), this::makeCustomer));
 
   }
 
   // -------------------- Defining actions the player can do -----------------------
 
-  public String askQuestion(String question){
+  public String askQuestion(String question, Scanner s){
     System.out.println(question);
     System.out.print("> ");
     String answer = s.nextLine();
@@ -35,45 +34,45 @@ public class Player {
     return answer;
   }
 
-  private Customer makeCustomer(){
-    String name = askQuestion("Enter name: ");
-    double weight = Double.parseDouble(askQuestion("Enter weight"));
-    double wealth = Double.parseDouble(askQuestion("Enter wealth?"));
-    String race = askQuestion("Enter name: ");
+  private Customer makeCustomer(Scanner s){
+    String name = askQuestion("Enter name: ", s);
+    double weight = Double.parseDouble(askQuestion("Enter weight", s));
+    double wealth = Double.parseDouble(askQuestion("Enter wealth?", s));
+    String race = askQuestion("Enter name: ", s);
     return new Customer(name, weight, wealth, race);
   }
 
-  private Baker makeBaker() {
-    String storeName = askQuestion("Enter Baker name: ");
-    double skill = Double.parseDouble(askQuestion("Enter Baker Skill: "));
-    int numCakes = Integer.parseInt(askQuestion("How many Cakes does store have: "));
+  private Baker makeBaker(Scanner s) {
+    String storeName = askQuestion("Enter Baker name: ", s);
+    double skill = Double.parseDouble(askQuestion("Enter Baker Skill: ", s));
+    int numCakes = Integer.parseInt(askQuestion("How many Cakes does store have: ", s));
 
     Cake[] cakes = new Cake[numCakes];
     int[] amounts = new int[numCakes];
 
     for (int i = 0; i < numCakes; i++) {
       System.out.printf("\n--- Making Cake %d ---\n", i + 1);
-      cakes[i] = makeCake();
+      cakes[i] = makeCake(s);
       amounts[i] = Integer.parseInt(askQuestion("How many of this cake? "));
     }
 
     return new Baker(cakes, amounts, storeName, skill);
   }
 
-  private Cake makeCake() {
-    String name = askQuestion("Cake name: ");
-    int count = Integer.parseInt(askQuestion("Enter amounts of ingredients: "));
+  private Cake makeCake(Scanner s) {
+    String name = askQuestion("Cake name: ", s);
+    int count = Integer.parseInt(askQuestion("Enter amounts of ingredients: ", s));
     String[] ingredients = new String[count];
     for (int i = 0; i < count; i++) {
       System.out.print("Ingredient " + (i + 1) + ": ");
       ingredients[i] = s.nextLine();
     }
 
-    double cost = Double.parseDouble(askQuestion("Cost? "));
+    double cost = Double.parseDouble(askQuestion("Cost? ", s));
     System.out.print("Weight: ");
-    double weight = Double.parseDouble(askQuestion("Weight? "));
+    double weight = Double.parseDouble(askQuestion("Weight? ", s));
     System.out.print("Quality (1–10): ");
-    double quality = Double.parseDouble(askQuestion("Quality? "));
+    double quality = Double.parseDouble(askQuestion("Quality? ", s));
 
     System.out.println("Now, create flour for this cake:");
     Flour flour = makeFlour();
@@ -81,23 +80,23 @@ public class Player {
     return new Cake(ingredients, cost, weight, name, flour, quality);
   }
 
-  private Flour makeFlour() {
-    String name = askQuestion("Flour name: ");
-    double weight = Double.parseDouble(askQuestion("Flour weight"));
+  private Flour makeFlour(Scanner s) {
+    String name = askQuestion("Flour name: ", s);
+    double weight = Double.parseDouble(askQuestion("Flour weight", s));
     int price = Integer.parseInt("Flour price (an int)");
     int quality = 10;
 
     return new Flour(name, weight, price, quality);
   }
 
-  private PTSA makePTSA(){
-    String name = askQuestion("Name of PTSA? ");
-    double wealth = Double.parseDouble(askQuestion("Initial wealth? "));
+  private PTSA makePTSA(Scanner s){
+    String name = askQuestion("Name of PTSA? ", s);
+    double wealth = Double.parseDouble(askQuestion("Initial wealth? ", s));
     double cut = Double.parseDouble("Cut the PTSA takes froms sales? ");
     Map<String, Double> needs = new HashMap<>();
     while (true){
       System.out.println("Enter something the PTSA needs (or 'done' when finsihed)");
-      String item = askQuestion("What is needed? ");
+      String item = askQuestion("What is needed? ", s);
       if (item.equalsIgnoreCase("done")){
         break;
       }
