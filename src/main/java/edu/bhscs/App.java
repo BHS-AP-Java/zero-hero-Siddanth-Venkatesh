65289f7254389ec7363925ab696a495159cebc12
2001875 <98480402+2001875@users.noreply.github.com>
2025-10-21 16:26:49 +0000

    I know how to make the app file

---
+++ src/main/java/edu/bhscs/App.java
@@ -0,0 +1,11 @@
+package edu.bhscs;
+
+public class App {
+  public static void main(String[] args){
+    date_01();
+    System.out.println("I am the app");
+  }
+  public static void date_01(){
+    System.out.println("today's work");
+  }
+}
